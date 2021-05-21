bf8b099751cfb7d6fb5fdb74882e7becf2dfa35e
AbdelrahmanElkhradely <65429772+AbdelrahmanElkhradely@users.noreply.github.com>
2021-05-21 18:37:00 +0200

    .

--- GetSegmentedCut.py
+++ GetSegmentedCut.py
@@ -8,7 +8,7 @@ def getSegmentedNcut(img,Kvalues):
   img_array = img.reshape(154401, 3)
   segmented_results = np.empty([len(Kvalues), 481, 321])
   for k, i in zip((Kvalues), range(len(Kvalues))):
-    seg =SpectralClustering(n_clusters=k, n_neighbors=5, assign_labels='kmeans',).fit(img_array)
+    seg =SpectralClustering(n_clusters=k, n_neighbors=5,).fit(img_array)
     segmented_results[i] = np.array([j for j in seg.labels_]).reshape(481, 321)
     plt.imshow(segmented_results[i])
     plt.show()
